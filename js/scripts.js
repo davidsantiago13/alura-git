1c174b2aac93695c208a1b3583daed7350d0cd2e
David Santiago Martinez Ramírez <david.santiago.13.mr@outlook.com>
2023-01-25 23:54:12 -0600

    Se agregaron ejemplos de querySelectorAll y getElementById

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ const heading = document.querySelector('.header__texto h2'); // 0 o 1 elementos
 heading.textContent = "Nuevo Titulo"
 
 //**querySelectorAll */
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);   
+
+enlaces[0].textContent = 'Nuevo Texto Para el enlace'
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 
 //**getElementById */
+
+const heading2 = document.getElementById('heading')
+console.log(heading2)
