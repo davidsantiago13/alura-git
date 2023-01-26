2bcef55f8af6ed72d34da3cea3f35fdd773002a2
David Santiago Martinez Ramírez <david.santiago.13.mr@outlook.com>
2023-01-25 23:27:52 -0600

    Se agregaron los archivos del proyecto para controlar versiones

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//**querySelector */
+const heading = document.querySelector('.header__texto h2'); // 0 o 1 elementos
+heading.textContent = "Nuevo Titulo"
+
+//**querySelectorAll */
+
+
+//**getElementById */
